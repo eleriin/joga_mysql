f149a4d6c01e1b3bb9508a72394bc2b96fd180a0
Eleriin Kurs <eleriin.kurs@voco.ee>
2025-02-07 09:47:35 +0200

    Display an article by slug

--- controllers/articles.js
+++ controllers/articles.js
@@ -8,7 +8,17 @@ const getAllArticles = (req, res)=>{
         })
     })
 }
+const getArticleBySlug = (req, res) =>{
+    let sql = `SELECT * FROM article WHERE slug="${req.params.slug}"`
+    db.query(sql, (error,result) => {
+        res.render('article',{
+        article: result
+        })
+    })
+
+}
 
 module.exports = {
-    getAllArticles
+    getAllArticles,
+    getArticleBySlug
 }
